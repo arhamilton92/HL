67861b70028ab6add97c1a8c5c1d482f7d89202f
Andrea Hamilton <andrea.r.hamilton92@gmail.com>
2021-10-07 16:18:53 -0400

    git push heroku main

--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -7,21 +7,21 @@ exports.showGallery = catchAsync(async (req, res, next) => {
 	const { user } = req.params;
 	console.log('gallery');
 	const galleryUrl = `https://www.deviantart.com/api/v1/oauth2/gallery/all?access_token=${req.daToken}&username=${user}&limit=20`;
-	const foldersUrl = `https://www.deviantart.com/api/v1/oauth2/gallery/folders?access_token=${req.daToken}&username=${user}`;
+	const profileUrl = `https://www.deviantart.com/api/v1/oauth2/user/profile/${user}?access_token=${req.daToken}`;
 	const gallery = await axios({
 		method: 'GET',
 		url: galleryUrl,
 	});
-	const folders = await axios({
+	const profile = await axios({
 		method: 'GET',
-		url: foldersUrl,
+		url: profileUrl,
 	});
 	res.status(200).json({
 		message: 'success',
 		gallery: gallery.data.results,
-		folders: folders.data.results,
+		profile: profile.data.user,
 		galleryUrl,
-		foldersUrl,
+		profileUrl,
 		token: req.daToken,
 	});
 });
