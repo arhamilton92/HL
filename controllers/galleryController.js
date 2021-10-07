38bb9b0d254eecfcc99895ca369594ca005bad6e
Andrea Hamilton <andrea.r.hamilton92@gmail.com>
2021-10-07 14:17:58 -0400

    console.log troubleshooting deployed app

--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -4,24 +4,17 @@ const axios = require('axios');
 const catchAsync = require('../utils/catchAsync');
 
 exports.showGallery = catchAsync(async (req, res, next) => {
-	const { user } = req.params
-	console.log(user)
+	const { user } = req.params;
+	console.log(user);
 	console.log('gallery');
 	const url = `https://www.deviantart.com/api/v1/oauth2/gallery/all?access_token=${req.daToken}&username=${user}&limit=20`;
-	try {
-		const gallery = await axios({
-			method: 'GET',
-			url,
-		});
-		res.status(200).json({
-			message: 'success',
-			gallery: gallery.data.results,
-			url: url
-		});
-    } catch (error) {
-		res.json({
-			error: error,
-			url: url
-		})
-	}
+	const gallery = await axios({
+		method: 'GET',
+		url,
+	});
+	res.status(200).json({
+		message: 'success',
+		gallery: gallery.data.results,
+		url: url,
+	});
 });
